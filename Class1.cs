d19693a4dfa6e308f288406670dcb3bf0dd6ac11
ahmed sherif <ahmedsherif9220@gmail.com>
2022-08-02 15:48:55 +0200

    create interface

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEP_Supports_distributor
+{
+    internal class Class1 : IMEPCurveHolder
+    {
+        public List<XYZ> SupportsLocation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void GetSupportLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JustifySupport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceSupport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IMEPCurveHolder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Diagnostics;
+
+
+namespace MEP_Supports_distributor
+{
+     interface IMEPCurveHolder
+     {
+         List<XYZ> SupportsLocation  { get; set; }
+        void GetSupportLocation();
+        void JustifySupport();
+        void PlaceSupport();
+         
+
+     }
+}
